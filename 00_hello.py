0f7bf737ad070e45af46e11b162185685c030a9d
AbrahamDev <aflorescastrejon@gmail.com>
2024-11-08 21:27:46 -0500

    Se crean los ficheros 00 y 01 del curso

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario: 
+# Nuestro Hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un 
+comentario 
+en varias 
+lineas
+"""
+
+# Consultar el tipo de dato 
+print(type("Soy un dato str")) # Tipo "str"
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
+
